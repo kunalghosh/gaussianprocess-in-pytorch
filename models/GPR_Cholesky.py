5bad52d49c1881a0462b0b5183f5b082a0784774
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-06-23 12:16:46 +0300

    Working version of Exact GPR using cholesky. Demo script to use GPR also updated. GP by optimizing hyperparameters overfits (noisestd goes to zero)

--- models/GPR_Cholesky.py
+++ models/GPR_Cholesky.py
@@ -1,10 +1,7 @@
-import pdb
 import torch
 import torch.nn as nn
 from torch.nn.init import uniform_
 import math
-import torch.distributions.constraints as constraints
-from torch.nn.functional import softplus
 
 
 class GPRCholesky(nn.Module):
@@ -22,7 +19,7 @@ class GPRCholesky(nn.Module):
         """
         self.x, self.y, self.N = X, y, X.shape[-2]
 
-        self.Kxx = self.kernel(X, X) + torch.eye(self.N) * 1e-5
+        self.Kxx = self.kernel(X, X)  # + torch.eye(self.N) * 1e-5
         jitter = torch.eye(self.N) * self.noisestd ** 2
         L = torch.cholesky(self.Kxx + jitter)
         a, _ = torch.solve(y.unsqueeze(0), L.unsqueeze(0))
@@ -35,7 +32,7 @@ class GPRCholesky(nn.Module):
         alpha, L = self.forward(X, y)
         const = -0.5 * self.N * torch.log(2 * torch.tensor(math.pi))
         data_fit = -0.5 * y.t() @ alpha
-        complexity = -torch.trace(L)
+        complexity = -torch.log(torch.diag(L)).sum()
         print(
             f"nll terms datafit : {data_fit.detach().numpy()},\n complexity : {complexity}"
         )
@@ -54,10 +51,11 @@ class GPRCholesky(nn.Module):
 
         m = Kt.transpose(-2, -1) @ alpha
         v, _ = torch.solve(Kt.unsqueeze(0), L.unsqueeze(0))
-        v = v.unsqueeze(0)
+        v = v.squeeze(0)
         S = Ktt - v.transpose(-2, -1) @ v
         S = S.squeeze()
-        print(m.shape, S.shape)
+        # adding noisestd to get the variance of P(y*|f*)
+        S = S + torch.eye(S.shape[0]) * self.noisestd ** 2
         if full_cov is False:
             S = torch.diag(S)
         return m, S
--- sgpr-simpleregression.py
+++ sgpr-simpleregression.py
@@ -6,8 +6,9 @@ import torch
 from collections import namedtuple
 import numpy as np
 from kernels import SquaredExp
-from models import GPR
+from models import GPR, SGPR, GPRCholesky
 from visualize import visualize1d
+from torch.nn.functional import softplus
 
 plt.ion()
 
@@ -18,22 +19,25 @@ consts.noisestd = 0.3
 
 
 # data generating function
-# def f(x):
-#     return 5 * np.exp(-.5 * x**2 / 1.3**2)
-
 def f(x):
-    return 2 * np.sin(x)
+    return 5 * np.exp(-.5 * x**2 / 1.3**2)
+
+# def f(x):
+#     return 2 * np.sin(x)
 
 # training data
 x = torch.linspace(-5, 5, consts.Ntrain).unsqueeze(-1)
 y = f(x)
 
+# x = (x - x.mean()) / x.std()
+
 y_noisy = y + torch.randn_like(y) * consts.noisestd
 x_test = torch.linspace(-5, 5, consts.Ntest).unsqueeze(-1)
 
+# x_test = ( x_test - x.mean() ) / x.std()
 # plt.scatter(x.numpy(), y_noisy.numpy())
 kernel = SquaredExp()
-model = GPR(kernel)
+model = GPRCholesky(kernel)
 
 # m, S = model(x, y)
 # print(f"m {m.shape}")
@@ -42,12 +46,13 @@ model = GPR(kernel)
 # visualize1d(x, y, y_noisy, x_test, m, var, "GPR-prior-regression.pdf")
 
 
-def train(model, x, y_noisy, n_iters=500, lr=1e-3):
+def train(model, x, y_noisy, n_iters=501, lr=1e-3):
     opt = torch.optim.Adam(model.parameters(), lr=lr)
     for epoch in range(n_iters):
         opt.zero_grad()
-        m, S = model.forward(x, y)
-        loss = -model.nll()
+        # model.forward(x, y)
+        # loss = -model.nll(x, y)
+        loss = model.loss(x, y)
         loss.backward()
         opt.step()
         print(f"iter {epoch}: Marginal log likelihood: {-loss.item()}")
@@ -56,12 +61,11 @@ def train(model, x, y_noisy, n_iters=500, lr=1e-3):
         print(f"noise std {model.noisestd.item()}")
         if not epoch % 50:
             m, S = model.predict(x_test, full_cov=False)
-            visualize1d(x, y, y_noisy, x_test, m, S, f"GPR-regression-{epoch}.pdf")
-
+            visualize1d(x, y, y_noisy, x_test, m, S, f"{model}-regression-{epoch}.pdf")
 
 
 train(model, x, y_noisy, n_iters=501, lr=1e-3)
 m, S = model.predict(x_test, full_cov=False)
 print(f"m {m.shape}")
 print(f"S {S.shape}")
-visualize1d(x, y, y_noisy, x_test, m, S, "GPR-regression.pdf")
+visualize1d(x, y, y_noisy, x_test, m, S, "{model}-regression.pdf")
