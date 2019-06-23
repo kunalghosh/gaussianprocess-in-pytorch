7e6f37504b76129f402e0a6b607be8b74e3793b4
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-06-23 23:00:56 +0300

    Working version of SGPR quite sensitive to Kernel lenghtscale initialization. Includes a working initialization of Kernel lenghtscale.

--- kernels/squared_exp.py
+++ kernels/squared_exp.py
@@ -8,8 +8,8 @@ class SquaredExp(nn.Module):
         super(SquaredExp, self).__init__()
         # self.lengthscale = nn.Parameter(torch.tensor(0.1))
         # self.prefactor = nn.Parameter(torch.tensor(1.))  # output std_dev
-        self.lengthscale = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), -3, 0)))
-        self.prefactor = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), -1, 1)))
+        self.lengthscale = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), -0.5, 1.0))) # 1, 2.5
+        self.prefactor = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), 0.5, 1.5)))
 
     def forward(self, X1, X2=None):
         if X2 is None:
@@ -25,3 +25,22 @@ class SquaredExp(nn.Module):
         l2 = ((X1 - X2.transpose(1, 2))**2).sum(dim=-1)
         exp_term = torch.exp(-0.5 * l2 / self.lengthscale**2)
         return (self.prefactor**2 * exp_term).squeeze()
+
+
+# class SquaredExp(nn.Module):
+#     def __init__(self, D_out=20):
+#         super(SquaredExp, self).__init__()
+#         self.lengthscale = nn.Parameter(uniform_(torch.empty(D_out, 1, 1), -1, -0.5))
+#         self.prefactor = nn.Parameter(uniform_(torch.empty(D_out, 1, 1), 0.5, 1.5))
+
+#     def forward(self, X, Z):
+#         gamma = torch.exp(self.lengthscale)
+#         if Z is None:
+#             Z = X
+#         scaled_X = X / gamma ** 2
+#         scaled_Z = Z / gamma ** 2
+#         X2 = (scaled_X ** 2).sum(-1, keepdim = True)
+#         Z2 = (scaled_Z ** 2).sum(-1, keepdim = True)
+#         XZ = scaled_X.matmul(scaled_Z.transpose(1, 2))
+#         r2 = (X2 - 2 * XZ + Z2.transpose(1, 2)).clamp(min = 1e-6)
+#         return self.prefactor ** 2 * torch.exp(-0.5 * r2)
--- models/SGPR.py
+++ models/SGPR.py
@@ -1,16 +1,15 @@
 import pdb
+import math
 
 import torch
 import torch.nn as nn
-from sklearn.cluster import KMeans
-from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.nn.init import uniform_
 
 from models import GPR
 
 
 class SGPR(GPR):
-    def __init__(self, kernel, M=20, D_out=1):
+    def __init__(self, kernel=None, M=20, Z=None, D_out=1):
         """
         :Args
         kernel : The gaussian process kernel.
@@ -22,66 +21,91 @@ class SGPR(GPR):
         self.noisestd = nn.Parameter(
             torch.exp(uniform_(torch.empty(1), 0., 1.)))
         # Variational mean and cholesky variance
-        self.qm = nn.Parameter(torch.empty(M, 1).uniform_(-3, 1))
+        self.qm = nn.Parameter(torch.empty(M, 1).normal_())
         self.qL = nn.Parameter(torch.empty(M, M).normal_()).tril()
-        self.z = None
+        if Z is None:
+            self.z = None
+        else:
+            self.z = nn.Parameter(Z)
         self.M = M
 
     def __str__(self):
         return "SGPR"
 
     def forward(self, x):
-        if self.z is None:
-            # self.z = torch.clone(x[:self.M, :])
-            kmeans = KMeans(n_clusters=self.M, random_state=0).fit(x)
-            self.z = torch.Tensor(kmeans.cluster_centers_)
+        # if self.z is None:
+        #     # self.z = torch.clone(x[:self.M, :])
+        #     kmeans = KMeans(n_clusters=self.M, random_state=0).fit(x)
+        #     self.z = nn.Parameter(torch.Tensor(kmeans.cluster_centers_))
+
+        N = x.shape[-2]
+        self.N = N
+        M = self.M
 
         z = self.z
-        Kxx = self.kernel(x, x)
+        Kxx = self.kernel(x, x) + torch.eye(N) * 1e-3
         Kxz = self.kernel(x, z)
-        Kzz = self.kernel(z, z)
+        Kzz = self.kernel(z, z) + torch.eye(M) * 1e-3
 
         self.Kzz = Kzz
 
         A = Kxz @ Kzz.inverse()
         m = A @ self.qm
-        S_q = self.qL.t() @ self.qL
+        S_q = self.qL @ self.qL.t()
         S = Kxx + A @ (S_q - Kzz) @ A.transpose(-2, -1)
         return m, S
 
     def nll(self, x, y):
-        N = x.shape[-2]
         # posterior mean and Variance
         m, S = self.forward(x)
-        mvn = MultivariateNormal(m, torch.eye(N) * self.noisestd**2)
-        logpdf = mvn.log_prob(y).sum()
+        # mvn = MultivariateNormal(m, torch.eye(N) * self.noisestd**2)
+        # logpdf = mvn.log_prob(y).sum()
+        N = self.N
+        k, _ = m.shape
+        logpdf = -0.5 * (
+            torch.sum(torch.log(self.noisestd**2)) +
+            (y - m).t() @ (torch.eye(N) * 1 / self.noisestd**2) @ (y - m) +
+            N * torch.log(2 * torch.tensor(math.pi))).squeeze()
         trace_term = -0.5 * (1 / self.noisestd**2) * torch.trace(S)
         return logpdf + trace_term
 
     def elbo(self, x, y):
-        return (self.nll(x, y) + self.kl())
+        nll = self.nll(x, y)
+        kl = self.kl()
+        print(f"nll {nll} kl {kl}")
+        return nll - kl
 
     def loss(self, x, y):
         return -self.elbo(x, y)
 
     def kl(self):
         return self.kl_multivariate_normal(self.qm,
-                                           self.qL.t() @ self.qL,
-                                           torch.zeros(self.M, 1), self.Kzz)
+                                           self.qL @ self.qL.t(),
+                                           torch.zeros(self.M, 1),
+                                           torch.eye(self.M))
 
     def kl_multivariate_normal(self, m0, S0, m1, S1):
         """
         KL between a gaussian N(m,S) and N(,I)
         KL(q||p)
         """
-        k, _ = m0.shape
+        M = self.M
+        print(f"k is {M}")
         trace_term = torch.trace(S1.inverse() @ S0)
         mean_term = (m1 - m0).t() @ S1.inverse() @ (m1 - m0)
-        log_det_term = torch.log(torch.det(S1)) - torch.log(torch.det(S0))
-        return 0.5 * (trace_term + mean_term - k + log_det_term)
+
+        log_det_S1 = torch.logdet(S1 + 1e-5 * torch.eye(M) * torch.randn(M)**2)
+        log_det_S0 = torch.logdet(S0 + 1e-5 * torch.eye(M) * torch.randn(M)**2)
+        log_det_term = log_det_S1 - log_det_S0
+        print(f"logdet S0 = {log_det_S0} S1 = {log_det_S1}")
+        print(
+            f"trace term {trace_term} mean_term {mean_term} logdet {log_det_term}"
+        )
+        return 0.5 * (trace_term + mean_term - M + log_det_term)
 
     def predict(self, xtest, full_cov=True):
+        N = self.N
         m, S = self.forward(xtest)
         if not full_cov:
-            S = torch.diag(S)
+            S = torch.diag(S) 
         return m, S
