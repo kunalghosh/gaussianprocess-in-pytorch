0f4e575b9656cee7be0a28dc7fe3497188a17a73
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-06-23 23:11:01 +0300

    Script with working SGPR regression demo code.

--- sgpr-simpleregression.py
+++ sgpr-simpleregression.py
@@ -1,16 +1,20 @@
-import matplotlib
 import pdb
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-import torch
 from collections import namedtuple
+
+import matplotlib
 import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+from torch.nn.functional import softplus
+
 from kernels import SquaredExp
 from models import GPR, SGPR, GPRCholesky
 from visualize import visualize1d
-from torch.nn.functional import softplus
 
-plt.ion()
+matplotlib.use('Agg')
+
+# plt.ion()
 
 consts = namedtuple("consts", "Ntrain Ntest noisestd")
 consts.Ntrain = 500
@@ -21,6 +25,7 @@ consts.noisestd = 0.3
 # data generating function
 def f(x):
     return 5 * np.exp(-.5 * x**2 / 1.3**2)
+
 
 # def f(x):
 #     return 2 * np.sin(x)
@@ -37,7 +42,10 @@ x_test = torch.linspace(-5, 5, consts.Ntest).unsqueeze(-1)
 # x_test = ( x_test - x.mean() ) / x.std()
 # plt.scatter(x.numpy(), y_noisy.numpy())
 kernel = SquaredExp()
-model = GPRCholesky(kernel)
+M = 20
+kmeans = KMeans(n_clusters=M, random_state=0).fit(x)
+z = torch.Tensor(kmeans.cluster_centers_)
+model = SGPR(kernel=kernel, M=M, Z=z, D_out=1)
 
 # m, S = model(x, y)
 # print(f"m {m.shape}")
@@ -59,13 +67,14 @@ def train(model, x, y_noisy, n_iters=501, lr=1e-3):
         print(f"Kernel lenghtscale {model.kernel.lengthscale.item()}")
         print(f"Kernel prefactor {model.kernel.prefactor.item()}")
         print(f"noise std {model.noisestd.item()}")
-        if not epoch % 50:
+        if not epoch % 500:
             m, S = model.predict(x_test, full_cov=False)
-            visualize1d(x, y, y_noisy, x_test, m, S, f"{model}-regression-{epoch}.pdf")
+            visualize1d(x, y, y_noisy, x_test, m, S,
+                        f"{model}-regression-{epoch}.pdf")
 
 
-train(model, x, y_noisy, n_iters=501, lr=1e-3)
+train(model, x, y_noisy, n_iters=5001, lr=5e-3)
 m, S = model.predict(x_test, full_cov=False)
 print(f"m {m.shape}")
 print(f"S {S.shape}")
-visualize1d(x, y, y_noisy, x_test, m, S, "{model}-regression.pdf")
+visualize1d(x, y, y_noisy, x_test, m, S, f"{model}-regression.pdf")
