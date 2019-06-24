bfb617a5f056ee48ab3c83ca6e26acea8b994adc
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-06-24 17:11:08 +0300

    Modified version of SGPR now works with D_out GPs in the same class.

--- models/SGPR_DGP.py
+++ models/SGPR_DGP.py
@@ -29,6 +29,7 @@ class SGPRDGP(SGPR):
         else:
             self.z = nn.Parameter(Z)
         self.M = M
+        self.D_out = D_out
 
     def __str__(self):
         return "SGPR_DGP"
@@ -52,22 +53,24 @@ class SGPRDGP(SGPR):
 
         A = Kxz @ Kzz.inverse()
         m = A @ self.qm
-        S_q = self.qL @ self.qL.t()
+        S_q = self.qL @ self.qL.transpose(-2, -1)
         S = Kxx + A @ (S_q - Kzz) @ A.transpose(-2, -1)
         return m, S
 
     def nll(self, x, y):
+        if y.ndimension() < 3:
+            y = y.reshape(self.D_out, -1, 1)
         # posterior mean and Variance
         m, S = self.forward(x)
         # mvn = MultivariateNormal(m, torch.eye(N) * self.noisestd**2)
         # logpdf = mvn.log_prob(y).sum()
         N = self.N
-        k, _ = m.shape
+        # k, _ = m.shape
         logpdf = -0.5 * (
             torch.sum(torch.log(self.noisestd**2)) +
-            (y - m).t() @ (torch.eye(N) * 1 / self.noisestd**2) @ (y - m) +
+            (y - m).transpose(-2, -1) @ (torch.eye(N) * 1 / self.noisestd**2) @ (y - m) +
             N * torch.log(2 * torch.tensor(math.pi))).squeeze()
-        trace_term = -0.5 * (1 / self.noisestd**2) * torch.trace(S)
+        trace_term = -0.5 * (1 / self.noisestd**2) * torch.einsum('kii', S)
         return logpdf + trace_term
 
     def elbo(self, x, y):
@@ -80,9 +83,10 @@ class SGPRDGP(SGPR):
         return -self.elbo(x, y)
 
     def kl(self):
-        return self.kl_multivariate_normal(self.qm, self.qL @ self.qL.t(),
-                                           torch.zeros(self.M, 1),
-                                           torch.eye(self.M))
+        D_out = self.D_out
+        return self.kl_multivariate_normal(self.qm, self.qL @ self.qL.transpose(-2, -1),
+                                           torch.zeros(D_out, self.M, 1),
+                                           eye(D_out, self.M))
 
     def kl_multivariate_normal(self, m0, S0, m1, S1):
         """
@@ -91,17 +95,17 @@ class SGPRDGP(SGPR):
         """
         M = self.M
         print(f"k is {M}")
-        trace_term = torch.trace(S1.inverse() @ S0)
-        mean_term = (m1 - m0).t() @ S1.inverse() @ (m1 - m0)
-
-        log_det_S1 = torch.logdet(S1 + 1e-5 * torch.eye(M) * torch.randn(M)**2)
-        log_det_S0 = torch.logdet(S0 + 1e-5 * torch.eye(M) * torch.randn(M)**2)
+        trace_term = torch.einsum('kii', S1.inverse() @ S0)
+        mean_term = ( (m1 - m0).transpose(-2, -1) @ S1.inverse() @ (m1 - m0) ).squeeze()
+        print(S1.shape)
+        log_det_S1 = torch.stack([torch.logdet(_) for _ in S1 + 1e-5 * torch.eye(M) * torch.randn(M)**2])
+        log_det_S0 = torch.stack([torch.logdet(_) for _ in S0 + 1e-5 * torch.eye(M) * torch.randn(M)**2])
         log_det_term = log_det_S1 - log_det_S0
         print(f"logdet S0 = {log_det_S0} S1 = {log_det_S1}")
         print(
             f"trace term {trace_term} mean_term {mean_term} logdet {log_det_term}"
         )
-        return 0.5 * (trace_term + mean_term - M + log_det_term)
+        return 0.5 * (trace_term + mean_term - M + log_det_term).squeeze()
 
     def predict(self, xtest, full_cov=True):
         N = self.N
