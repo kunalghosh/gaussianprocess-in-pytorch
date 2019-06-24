a1b1de6002af5e808ecfd62c24f5d481ca200edb
Kunal Ghosh <kunal.ghosh@aalto.fi>
2019-06-24 11:50:05 +0300

    Implementation of multidimensional identity matrix.

---
+++ utils/eye.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+
+
+def eye(*args, toTorch=True):
+    """
+    returns an identity matrix with given dimensions
+    Last dimension is always the size of the identity matrix
+    eye(5, 6) -> 5, 6x6 Identity matrices, out dim = [5, 6, 6] 
+    eye(4, 5, 6) -> tensor of 4, 5, 6x6 Identity matrices, out dim = [4, 5, 6, 6]
+
+    Continuing from the last example, the implementation creates a 6x6 identity matrix and tiles i
+    """
+    assert_vals = [isinstance(_, int) for _ in args]
+    assert all(
+        assert_vals
+    ), "To construct a multi-dimensional Identity matrix, each dimension must be an integer."
+
+    eye = np.eye(args[-1])
+    multidim_eye = np.tile(
+        eye, (*args[:-1], 1, 1))  # (4, 5, 1, 1) following docstring example
+    if toTorch:
+        multidim_eye = torch.tensor(multidim_eye)
+    return multidim_eye
